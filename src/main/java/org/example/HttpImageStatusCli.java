c4911cb8a4aee219c0bbf0e4f741cc75450268af
Yevhen Petrovytskyi <e.petrovitsky@gmail.com>
2023-03-05 01:06:10 +0100

    HttpImageStatusCli added

--- src/main/java/org/example/HttpImageStatusCli.java
+++ src/main/java/org/example/HttpImageStatusCli.java
@@ -1,2 +1,24 @@
-package org.example;public class HttpImageStatusCli {
+package org.example;
+import java.util.Scanner;
+
+public class HttpImageStatusCli {
+    public void askStatus() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter HTTP status code:");
+        String input = scanner.nextLine();
+
+        try {
+            int code = Integer.parseInt(input);
+            String imageUrl = new HttpStatusChecker().getStatusImage(code);
+            new HttpStatusImageDownloader().downloadStatusImage(code);
+            System.out.println("Image for HTTP status " + code + " downloaded from " + imageUrl);
+        } catch (NumberFormatException e) {
+            System.out.println("Please enter valid number");
+            askStatus();
+        } catch (Exception e) {
+            System.out.println("There is not image for HTTP status " + input);
+            askStatus();
+        }
+    }
 }
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,31 +3,9 @@ package org.example;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-
-        HttpStatusChecker checker = new HttpStatusChecker();
-
-        String imageUrl = checker.getStatusImage(200);
-        System.out.println("Image URL: " + imageUrl);
-
-        String imageUrl1 = checker.getStatusImage(300);
-        System.out.println("Image URL1: " + imageUrl1);
-
-        try {
-            String imageUrl2 = checker.getStatusImage(10000);
-            System.out.println("Image URL2: " + imageUrl2);
-        } catch (RuntimeException ex) {
-            System.out.println(ex.getMessage());
-        }
-
-        HttpStatusImageDownloader downloader = new HttpStatusImageDownloader();
-        downloader.downloadStatusImage(200);
-        downloader.downloadStatusImage(101);
-        try {
-            downloader.downloadStatusImage(10000);
-        } catch (RuntimeException e) {
-            System.out.println("Exception caught: " + e.getMessage());
-        }
+    public static void main(String[] args) {
+        HttpImageStatusCli cli = new HttpImageStatusCli();
+        cli.askStatus();
     }
 
 
